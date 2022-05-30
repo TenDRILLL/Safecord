d092cf40df521cab69821582efe259b5eea9f356
TenDRILLL <sofyeer@gmail.com>
2022-05-30 15:39:02 +0300

    Implement loading commands in ready event.

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,10 +1,19 @@
+import {readdirSync} from "fs";
+
 class Ready extends require("../classes/Event"){
     constructor() {
         super("ready",true);
     }
 
     exec(bot){
-        console.log("Ready!");
+        console.log("Ready event, loading commands.");
+        bot.commands = new Map();
+        readdirSync("./commands").forEach(f => {
+            if(!f.endsWith(".js")) return;
+            const js = require(`../commands/${f}`);
+            if(!(js instanceof require("../classes/Command"))) return;
+            bot.commands.set(js.getName(),js);
+        });
     }
 }
 module.exports = new Ready();
