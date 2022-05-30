58b589c0f1a0a481700a39d5f1ee5692a12a3198
TenDRILLL <sofyeer@gmail.com>
2022-05-30 05:19:29 +0300

    Create command deploy script.

---
+++ src/deployCommands.ts
@@ -0,0 +1,19 @@
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const {readdirSync} = require("fs");
+
+const commands = [];
+
+readdirSync("./commands").forEach(f => {
+    if(!f.endsWith(".js")) return;
+    const js = require(`./commands/${f}`);
+    if(!(js instanceof require("./classes/Command"))) return;
+    if(js.getSlashObject() === null) return;
+    commands.push(js.getSlashObject());
+});
+
+const rest = new REST({ version: '9' }).setToken(require("./config.json").token);
+
+rest.put(Routes.applicationCommands(require("./config.json").applicationId), { body: commands })
+    .then(() => console.log('Successfully registered application commands.'))
+    .catch(console.error);
