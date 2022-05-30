99282ba52f7ca0416759e7da65361da7d7841eb0
TenDRILLL <sofyeer@gmail.com>
2022-05-30 15:39:17 +0300

    Implement enmap in index

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 const { Client, GatewayIntentBits } = require("discord.js");
+const Enmap = require("enmap");
 const bot = new Client({
     intents: [
         GatewayIntentBits.Guilds
     ]
 });
 const { token } = require("./config.json");
+bot.db = new Enmap(
+    {
+        name: "configurations"
+    }
+);
 
 require("./automation/createEvents").exec(bot);
 
