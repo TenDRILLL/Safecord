6ebdcdb67632899aa891e7a3a30d23a6ea864f78
TenDRILLL <sofyeer@gmail.com>
2022-05-31 17:18:29 +0300

    Implement translator, and also defer guild configs

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -6,13 +6,22 @@ class Ready extends require("../classes/Event"){
     }
 
     exec(bot){
-        console.log("Ready event, loading commands.");
+        console.log("Ready event, loading commands...");
         bot.commands = new Map();
         readdirSync("./commands").forEach(f => {
             if(!f.endsWith(".js")) return;
             const js = require(`../commands/${f}`);
             if(!(js instanceof require("../classes/Command"))) return;
             bot.commands.set(js.getName(),js);
+            console.log(`${js.getName()} loaded`);
+        });
+        console.log("\nLoading languages...");
+        require("../automation/translator").loadLanguages();
+        console.log("\nLoading Guild Configurations...");
+        bot.guilds.fetch().then(() => {
+            bot.db.defer.then(()=>{
+                console.log(`Captcha-Bot operational, observing ${bot.db.size} configurations in ${bot.guilds.cache.size} guilds.`);
+            });
         });
     }
 }
