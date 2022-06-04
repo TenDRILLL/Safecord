1c0314e648e7d4b2a769699292d3f0cfa3c8556b
TenDRILLL <sofyeer@gmail.com>
2022-06-04 18:18:00 +0300

    Planning

---
+++ src/classes/CaptchaModal.ts
@@ -0,0 +1,13 @@
+class CaptchaModal {
+    /*TODO: Plan what values are required, should have:
+    * CODE
+    * DESCRIPTION
+    * ??
+    */
+
+    constructor(){
+
+    }
+}
+
+module.exports = CaptchaModal;
---
+++ src/commands/captcha.ts
@@ -0,0 +1,9 @@
+class Captcha extends require("../classes/Command"){
+    constructor() {
+        super("captcha",{
+
+        });
+    }
+    //TODO: Handle button + modalSubmit + granting role here.
+}
+module.exports = new Captcha();
