46c1b2c36458b4cd9abd5cceb9623197ffb1022b
TenDRILLL <sofyeer@gmail.com>
2022-06-15 14:08:10 +0300

    Fix a bunch of stupid imports

--- src/deployCommands.ts
+++ src/deployCommands.ts
@@ -1,22 +1,23 @@
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
-const { readdirSync } = require("fs");
-const { CommandApplicationData } = require("discord.js");
-// @ts-ignore
-const token = process.env.token ?? require("./config.json").token;
-const applicationId = process.env.applicationId ?? require("./config.json").applicationId;
-const commands: typeof CommandApplicationData[] = [];
+import { REST } from '@discordjs/rest';
+import { Routes } from 'discord-api-types/v9';
+import { readdirSync } from "fs";
+import { ApplicationCommandData } from "discord.js";
+import { Command } from "./classes/Command";
+import * as config from "./config.json";
+
+const token = process.env.token ?? config.token;
+const applicationId = process.env.applicationId ?? config.applicationId;
+const commands: ApplicationCommandData[] = [];
 
 readdirSync("./commands").forEach(f => {
     if(!f.endsWith(".js")) return;
     const js = require(`./commands/${f}`);
-    if(!(js instanceof require("./classes/Command"))) return;
-    if(!(js.getSlashObject())) return;
+    if(!(js instanceof Command) || !(js.getSlashObject())) return;
     commands.push(js.getSlashObject());
 });
 
 const rest = new REST({ version: '9' }).setToken(token);
 
-rest.put(Routes.applicationCommands(applicationId), { body: commands })
+rest.put(Routes.applicationCommands(applicationId as string), { body: commands })
     .then(() => console.log('Successfully registered application commands.'))
     .catch(console.error);
