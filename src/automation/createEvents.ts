8d6567a926e8cd23d00b72cb438a8ac6de6aa97a
TenDRILLL <sofyeer@gmail.com>
2022-06-16 16:06:13 +0300

    Fix import

--- src/automation/createEvents.ts
+++ src/automation/createEvents.ts
@@ -1,9 +1,10 @@
 import { readdirSync } from "node:fs";
+import { Event } from "../classes/Event";
 export function exec(bot){
     const eventFiles = readdirSync(`${__dirname}/../events`).filter(x => x.endsWith(".js"));
     eventFiles.forEach(name => {
         const file = require(`../events/${name}`);
-        if(!(file instanceof require("../classes/Event"))) return;
+        if(!(file instanceof Event)) return;
         bot[file.isRunOnce() ? "once" : "on"](file.getName(), (...args) => file.exec(...args, bot));
     });
 }
